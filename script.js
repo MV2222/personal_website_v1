019ba1151d7c7f7c0e05c50d8b6730c0eef54a05
Mahadev Prasad Pal <109690667+MV2222@users.noreply.github.com>
2024-01-13 22:36:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+// Set current year
+const year = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log("currentYear");
+year.textContent = currentYear;
+
+// Make mmobile navigation work
+const mobileNav = document.querySelector(".mobile-nav");
+const header = document.querySelector(".header");
+
+mobileNav.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
+
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("section-name"))
+      header.classList.toggle("nav-open");
+  });
+});
+
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
